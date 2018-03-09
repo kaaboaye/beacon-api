91929bf434af1d470fdc9861843f382ae127b210
kaaboaye <kaaboaye@gmail.com>
2018-03-09 11:11:46 +0100

    ts

--- built/Config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: 3001,
-    routePrefix: '/api'
-};
-//# sourceMappingURL=Config.js.map
--- built/Controllers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Users_1 = require("./Users");
-var Sessions_1 = require("./Sessions");
-exports.default = Users_1.default.concat(Sessions_1.default);
-//# sourceMappingURL=index.js.map
