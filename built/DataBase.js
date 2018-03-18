b60a617cb8d18b6512797cae105b67591f473099
kaaboaye <kaaboaye@gmail.com>
2018-03-18 14:31:50 +0100

    pull from azure

--- built/DataBase.js
+++ built/DataBase.js
@@ -5,32 +5,13 @@ var Session_1 = require("./entity/Session");
 var ShortToken_1 = require("./entity/ShortToken");
 var Beacon_1 = require("./entity/Beacon");
 var BeaconLocation_1 = require("./entity/BeaconLocation");
-// export const ConnectionConfig: ConnectionOptions = {
-//   type: "postgres",
-//   host: "h.kaye.ovh",
-//   username: "postgres",
-//   password: "mysecretpassword",
-//   database: "postgres",
-//   port: 32768,
-//   ssl: false,
-//   synchronize: true,
-//   logging: true,
-//   entities: [
-//     User,
-//     Session,
-//     ShortToken,
-//     Beacon,
-//     BeaconLocation
-//   ]
-// };
 exports.ConnectionConfig = {
     type: "postgres",
-    host: "beaconsocialdb.postgres.database.azure.com",
-    username: "mieszko@beaconsocialdb",
-    password: "Zxcvbnm123",
+    host: "localhost",
+    username: "postgres",
+    password: "mysecretpassword",
     database: "postgres",
-    port: 5432,
-    ssl: true,
+    port: 32000,
     synchronize: true,
     logging: true,
     entities: [
--- src/DataBase.ts
+++ src/DataBase.ts
@@ -5,34 +5,13 @@ import {ShortToken} from "./entity/ShortToken";
 import {Beacon} from "./entity/Beacon";
 import {BeaconLocation} from "./entity/BeaconLocation";
 
-// export const ConnectionConfig: ConnectionOptions = {
-//   type: "postgres",
-//   host: "h.kaye.ovh",
-//   username: "postgres",
-//   password: "mysecretpassword",
-//   database: "postgres",
-//   port: 32768,
-//   ssl: false,
-//   synchronize: true,
-//   logging: true,
-//   entities: [
-//     User,
-//     Session,
-//     ShortToken,
-//     Beacon,
-//     BeaconLocation
-//   ]
-// };
-
-
 export const ConnectionConfig: ConnectionOptions = {
   type: "postgres",
-  host: "beaconsocialdb.postgres.database.azure.com",
-  username: "mieszko@beaconsocialdb",
-  password: "Zxcvbnm123",
+  host: "localhost",
+  username: "postgres",
+  password: "mysecretpassword",
   database: "postgres",
-  port: 5432,
-  ssl: true,
+  port: 32000,
   synchronize: true,
   logging: true,
   entities: [
